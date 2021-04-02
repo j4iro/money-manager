a48cc42e277d23bc68c5fd490366a2bcd0dacb28
j4iro <jlachirap.8@gmail.com>
2021-04-02 17:54:45 -0500

    add variables in config.js for auth

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,11 @@ const config = {
   port: process.env.PORT || 5000,
   mainLogs: process.env.MAIN_LOGS || 'src/logs/default.log',
   urlMongoConnection: process.env.URL_MONGO_CONNECTION || false,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
 }
 
 module.exports = { config }
